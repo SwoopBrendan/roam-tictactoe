46501edabae84f9eb6551830fb39e45eb7e46754
Brendan <brendan.vandermeulen@double-eye.com>
2018-04-25 15:24:41 +0200

    Cell and Grid components created

---
+++ src/components/Cell.vue
@@ -0,0 +1,39 @@
+<template>
+    <td class="cell" @click="strike">{{ mark }}</td>
+</template>
+
+<style>
+    .cell {
+        width: 33.333%;
+        height: 90px;
+        border: 6px solid #2c3e50;
+        font-size: 3.5em;
+    }
+
+    .cell:hover {
+        background-color: lightblue;
+    }
+
+    .cell::after {
+        content: '';
+        display: block;
+    }
+
+    .cell:first-of-type {
+        border-left-color: transparent;
+        border-top-color: transparent;
+    }
+
+    .cell:nth-of-type(2) {
+        border-top-color: transparent;
+    }
+
+    .cell:nth-of-type(3) {
+        border-right-color: transparent;
+        border-top-color: transparent;
+    }
+
+    tr:nth-of-type(3) .cell {
+        border-bottom-color: transparent;
+    }
+</style>
---
+++ src/components/Grid.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <table class="grid">
+            <tr>
+                <cell name="1"></cell>
+                <cell name="2"></cell>
+                <cell name="3"></cell>
+            </tr>
+            <tr>
+                <cell name="4"></cell>
+                <cell name="5"></cell>
+                <cell name="6"></cell>
+            </tr>
+            <tr>
+                <cell name="7"></cell>
+                <cell name="8"></cell>
+                <cell name="9"></cell>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style>
+    .grid {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    .gameStatus {
+        color: black;
+        margin-bottom: 30px;
+        padding: 15px;
+        font-size: 1.4em;
+        font-weight: bold;
+    }
+</style>
